128a9a5d235c8abef2ab31ecb53ab46273c13a0b
lhwda1388 <lhwda1388@naver.com>
2020-03-09 13:39:21 +0900

    NettyServer Sample

---
+++ src/main/java/com/sample/nss/NettyServerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sample.nss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyServerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyServerSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/nss/NettyServerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.nss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyServerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
